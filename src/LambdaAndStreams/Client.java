bed06f2781eed1201e6750b9646ed9caf0bdddda
aman <amankrsingh.2012@gmail.com>
2024-07-04 00:21:19 +0530

    Lambdas and Streams

--- src/LambdaAndStreams/Client.java
+++ src/LambdaAndStreams/Client.java
@@ -1,8 +1,8 @@
 package LambdaAndStreams;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -118,7 +118,7 @@ public class Client {
             return elem.psp>50;
         }).collect(Collectors.toList());
 
-        System.out.println(l3);
+//        System.out.println(l3);
 
         // map : Intermediate Method
 
@@ -140,8 +140,86 @@ public class Client {
                 }).sorted((o1,o2)->o2-o1).
                 collect(Collectors.toList());
 
-        System.out.println(l4);
-        System.out.println(l2);
+//        System.out.println(l4);
+//        System.out.println(l2);
+
+//        List<Integer> l2 = List.of(5 , 10 ,2 ,1 ,16 , 3 ,8 , 1 , 2 , 2);
+        // Find First -> Terminal
+//        Optional<Integer> op1 = l2.stream().
+//                filter(elem -> {
+//                    System.out.println("Filtering "+elem);
+//                    return (elem%2==0);
+//                }).map( (elem) -> {
+//                    System.out.println("Mapping "+elem);
+//                    return elem*elem;
+//                }).findFirst();
+
+//        if(op1.isPresent()){
+//            System.out.println(op1.get());
+//        }
+
+        //        List<Integer> l2 = List.of(5 , 10 ,2 ,1 ,16 , 3 ,8 , 1 , 2 , 2);
+//        int count =
+//                (int) l2.stream().filter(e -> {
+//                    System.out.println("Counting "+e);
+//                    return e>2;}).count();
+//
+//        System.out.println(count);
+
+        List<Integer> l5 = List.of(4,7,2,6);
+
+        Stream<Integer> test = l5.stream().map(e -> e*e);
+//         find sum of a list
+//        int sum = 0;
+//        for(Integer elem : l5) {
+
+//            sum += elem;
+//        }
+
+        // reduce is a terminal Function
+        // it tries to converge your whole stream into a single element based on some logic
+//        Integer x9 = l5.stream().reduce(0,(cur_sum,elem)->{
+//            cur_sum+=elem;
+//            return cur_sum;
+//        });
+
+//        System.out.println(x9);
+
+        // Try calculating Max
+
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5),
+                Arrays.asList(6, 7, 8)
+        );
+
+        // nested list
+        System.out.println(listOfLists);
+
+//        List<Integer> ans = listOfLists.stream().flatMap(elem -> {
+//            return elem.stream();
+//        }).collect(Collectors.toList());
+
+//        List<Integer> ans = listOfLists.stream().flatMap(elem -> {
+//            return elem.stream().map(elem1 -> elem1*elem1);
+//        }).collect(Collectors.toList());
+//
+//        System.out.println(ans);
+
+//        Predicate
+
+
+//        l2.stream().filter(elem-> elem%2==0).findFirst();
+        List<Integer> l6 = List.of(4,7,2,6);
+        Stream<Integer> stnew = l6.stream();
+        stnew = stnew.distinct();
+        stnew = stnew.filter(elem ->{
+            System.out.println("Print Hello World");
+            return elem%2==0;
+        });
+
+        Optional<Integer> test2 = stnew.findFirst();
+        System.out.println(test2);
 
 
     }
