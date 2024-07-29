4d2434a1629e54dcc419df8367905e984c55cab7
aman <amankrsingh.2012@gmail.com>
2024-07-29 22:28:27 +0530

    Observer Template

---
+++ src/Observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package Observer;
+
+public interface DisplayElement {
+	public void display();
+}
---
+++ src/Observer/WeatherStation.java
@@ -0,0 +1,8 @@
+package Observer;
+
+public class WeatherStation {
+
+	public static void main(String[] args) {
+
+	}
+}
